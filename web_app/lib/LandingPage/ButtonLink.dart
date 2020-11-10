b4525cd8e028298519fb0b488d987d55bc5976ec
ratnesh.shukla <ratneshkumarshukla07@gmail.com>
2020-11-10 20:20:31 +0700

    Flutter | Day2 | Layoutbuilder added

--- web_app/lib/LandingPage/ButtonLink.dart
+++ web_app/lib/LandingPage/ButtonLink.dart
@@ -12,18 +12,23 @@ class ButtonLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
-      child: SizedBox(
-        width: width > 680 ? 680 : width * .9,
-        child: FlatButton(
-          padding: EdgeInsets.symmetric(vertical: 32),
-          onPressed: () => launch(url),
-          child: Text(title),
-          color: Colors.teal,
-        ),
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final width = constraints.maxWidth;
+
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: SizedBox(
+            width: width > 680 ? 680 : width * .9,
+            child: FlatButton(
+              padding: EdgeInsets.symmetric(vertical: 32),
+              onPressed: () => launch(url),
+              child: Text(title),
+              color: Colors.teal,
+            ),
+          ),
+        );
+      },
     );
   }
 }
